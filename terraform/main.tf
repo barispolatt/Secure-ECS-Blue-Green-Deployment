2ebb2b5f0f1aed90113e92b5db05cf00d8aad1b0
barispolatt1999 <barispolatt1999@gmail.com>
2026-01-18 19:47:55 +0300

    main.tf orchestration configurations and load balancer map

--- terraform/main.tf
+++ terraform/main.tf
@@ -109,3 +109,53 @@ resource "aws_ecs_service" "app" {
     ignore_changes = [task_definition, load_balancer]
   }
 }
+
+# Orchestration
+resource "aws_codedeploy_app" "app" {
+  compute_platform = "ECS"
+  name             = "bluegreen-demo-app"
+}
+
+resource "aws_codedeploy_deployment_group" "dg" {
+  app_name               = aws_codedeploy_app.app.name
+  deployment_group_name  = "bluegreen-demo-dg"
+  service_role_arn       = aws_iam_role.codedeploy.arn
+  
+  # Blue/Green configurations
+  deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL" 
+    deployment_type   = "BLUE_GREEN"
+  }
+
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT" # Transfer traffic to Prod automatically
+    }
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 5 # to dont terminiate blue container directly, so we can rollback if needed
+    }
+  }
+
+  ecs_service {
+    cluster_name = aws_ecs_cluster.main.name
+    service_name = aws_ecs_service.app.name
+  }
+
+  load_balancer_info {
+    target_group_pair_info {
+      prod_traffic_route {
+        listener_arns = [aws_lb_listener.prod.arn]
+      }
+      test_traffic_route {
+        listener_arns = [aws_lb_listener.test.arn]
+      }
+      target_group {
+        name = aws_lb_target_group.blue.name
+      }
+      target_group {
+        name = aws_lb_target_group.green.name
+      }
+    }
+  }
+}
